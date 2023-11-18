fdb9fafebbb194b26018544715828f6837159e13
1-1-brk <anton.a.brakhnov@gmail.com>
2023-11-18 14:07:32 +0100

    some frontend changes

--- web3/contracts/mytoken.sol
+++ web3/contracts/mytoken.sol
@@ -5,10 +5,25 @@ pragma solidity ^0.8.20;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import {ERC20Capped} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
 
+
 contract MyToken is ERC20, ERC20Permit, ERC20Votes {
-    constructor() ERC20("MyToken", "MTK") ERC20Permit("MyToken") {}
+    mapping(address => bool) public hasMintedAirdrop;
+
+    constructor(uint256 cap, uint256 airdrop) ERC20("MusicChef", "MC") ERC20Capped(cap * (10 ** decimals())) ERC20Permit("MusicChef") {
+        // Mint the airdrop for the contract deployer
+        _mint(msg.sender, airdrop * (10 ** decimals()));
+    }
+
+    // Mint the airdrop for the caller if they haven't already minted it
+    function mintAirdrop() external {
+        require(!hasMintedAirdrop[msg.sender], "Airdrop already minted for this address");
+        
+        _mint(msg.sender, airdrop * (10 ** decimals()));
+        hasMintedAirdrop[msg.sender] = true;
+    }
 
     // The functions below are overrides required by Solidity.
 
